2537783fb8b4dd0ff94d9141566578242ea42875
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-21 16:57:04 +0900

    ログインAPI仮実装

--- vapor/MapBoxTestVapor/Sources/App/Controllers/UserController.swift
+++ vapor/MapBoxTestVapor/Sources/App/Controllers/UserController.swift
@@ -9,17 +9,27 @@ import Vapor
 
 struct UserController {
     func login(req: Request) throws -> EventLoopFuture<Response> {
-        let user = try req.content.decode(BaseUser.self)
-        //        let user = User.init(account: "enki", password: "aaa")
+//        let user = try req.content.decode(BaseUser.self)
+        let queryMailaddress: String? = req.query["mailaddress"]
+        let queryPassword: String? = req.query["password"]
+        guard let mailaddress = queryMailaddress,
+            let password = queryPassword else {
+                throw Abort(HTTPResponseStatus.badRequest)
+        }
         return User.query(on: req.db)
-            .filter(\.$mailAddress, .equal, user.mailaddress).all()
-            .flatMap{$0.encodeResponse(status: .ok, for: req)}
+            .filter(\.$mailAddress, .equal, mailaddress)
+            .filter(\.$passWord, .equal, password).first()
+            .unwrap(or: Abort(.forbidden, reason: "username or password incorrect"))
+            .flatMap{
+                $0.encodeResponse(status: .ok, for: req)
+                
+        }
     }
 }
 
 extension UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        routes.get("login", use: login)
+        routes.post("login", use: login)
     }
 }
 

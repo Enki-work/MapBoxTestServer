79804be05e6204108af94fa78f73654f65e8d82a
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-30 12:00:25 +0900

    環境整理

--- vapor/MapBoxTestVapor/Sources/App/configure.swift
+++ vapor/MapBoxTestVapor/Sources/App/configure.swift
@@ -6,24 +6,26 @@ import FluentMySQLDriver
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    
-    var mysqlHost: String = "127.0.0.1"
-    var mysqlPort: Int = 33060
-    var mysqlDB: String = "vapor"
-    var mysqlUser: String = "vapor"
-    var mysqlPass: String = "vapor"
-    
+
+    var mysqlHost: String = Environment.get("MYSQL_HOST") ?? "127.0.0.1"
+    var mysqlPort: Int = Int(Environment.get("MYSQL_PORT") ?? "33060") ?? 33060
+    let mysqlDB: String = Environment.get("MYSQL_DB") ?? "vapor"
+    let mysqlUser: String = Environment.get("MYSQL_USER") ?? "vapor"
+    let mysqlPass: String = Environment.get("MYSQL_PASS") ?? "vapor"
     switch app.environment {
     case .development, .testing:
+        app.logger.info("development, testing env")
+        app.logger.info("mysqlHost:\(mysqlHost) mysqlPort:\(mysqlPort)--- \(Environment.get("MYSQL_HOST")!)")
         app.http.server.configuration.port = 8080
         app.http.server.configuration.hostname = "0.0.0.0"
     case .production:
-        print("Under production env")
-        mysqlHost = Environment.get("MYSQL_HOST") ?? "127.0.0.1"
+        app.logger.info("production env")
+    case .custom(name: "Xcode"):
+        mysqlHost = "127.0.0.1"
+        app.logger.info("Xcode env")
+        app.http.server.configuration.port = 8081
+        app.http.server.configuration.hostname = "0.0.0.0"
         mysqlPort = 33060
-        mysqlDB = Environment.get("MYSQL_DB") ?? "vapor"
-        mysqlUser = Environment.get("MYSQL_USER") ?? "vapor"
-        mysqlPass = Environment.get("MYSQL_PASS") ?? "vapor"
     default:
         break
     }
